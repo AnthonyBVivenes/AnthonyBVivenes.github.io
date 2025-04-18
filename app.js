0ad9507d9cce31f5358a4533507996a92a4e9c26
AnthonyBrayanVivenes <35461970+AnthonyBVivenes@users.noreply.github.com>
2025-04-18 19:29:53 -0400

    Remodelacion parte 4

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ btn1.addEventListener("click", function(){
 setInterval(() => {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    //alert(`Resolución de la ventana: ${width}x${height}`);
-}, 10000); // 10000 milisegundos = 10 segundos
+    alert(`Resolución de la ventana: ${width}x${height}`);
+}, 5000); // 10000 milisegundos = 10 segundos
 
+
+alert("dddd")
