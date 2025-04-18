d5ea4e49839cbceb9a248d6894a62b856fbf2391
AnthonyBrayanVivenes <35461970+AnthonyBVivenes@users.noreply.github.com>
2025-04-18 17:55:58 -0400

    remodelación parte 1

---
+++ app.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector("#container-proyectos article .container-tag");
+    const tags = container.querySelectorAll("p");
+    function adjustTags() {
+        const containerWidth = container.clientWidth;
+        let usedWidth = 0;
+
+        tags.forEach(tag => {
+            tag.style.display = "block"; 
+            const tagWidth = tag.offsetWidth + 3;
+            if (usedWidth + tagWidth <= containerWidth) {
+                usedWidth += tagWidth;
+            } else {
+                tag.style.display = "none";
+            }
+        });
+    }
+    adjustTags();
+    window.addEventListener("resize", adjustTags);
+});
+
+
+const btn1 = document.getElementById("btnAppTasaDeCambios");
+
+btn1.addEventListener("click", function(){
+    window.open("https://github.com/AnthonyBVivenes/Convertidor-de-Bolivares", "_blank");
+});
+
+
+
+
+
+setInterval(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    //alert(`Resolución de la ventana: ${width}x${height}`);
+}, 10000); // 10000 milisegundos = 10 segundos
+
